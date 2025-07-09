2b2655fa115b9c66b1718713eec69f7198283b06
Agathe Boncompain <agathe.boncompain@gmail.com>
2025-07-09 14:56:28 +0200

    Gestion des erreurs : connexion à la BDD

--- config/db.js
+++ config/db.js
@@ -1,16 +1,24 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-dotenv.config()
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+import { error, log } from "console";
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+try {
+  const connection = await pool.getConnection();
+  console.log("Connecté à la base de données !");
+  connection.release();
+} catch (err) {
+  console.error("Erreur de connexion :", err.message);
+}
 
 export default pool;
-
